4475020834979abf1c50a8f9535488e7e444098e
vsmk-common-git <162783897+vsmk-common-git@users.noreply.github.com>
2024-03-13 23:11:52 +0530

    Added few programs

--- SamplePrograms/src/com/patterns/PatternRevPyramid.java
+++ SamplePrograms/src/com/patterns/PatternRevPyramid.java
@@ -1,13 +1,13 @@
 package com.patterns;
 
-public class PatternPyramid {
+public class PatternRevPyramid {
 
 	public static void main(String[] args) {
 		for(int i=1;i<=4;i++)
 		 {
-		 for(int j=4;j>=1;j--)
+		 for(int j=1;j<=4;j++)
 		 {
-		 if(i>=j)
+		 if(i<=j)
 		 {
 		 System.out.print("* ");
 		 }
@@ -16,6 +16,7 @@ public class PatternPyramid {
 		 }}
 		 System.out.println();
 		 }
+
 	}
 
 }
